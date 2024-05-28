f9d6fbc8e78cc41d7407ee54f75e3594becede21
kdk0411 <wnsldjqja@gmail.com>
2024-05-28 19:41:34 +0900

    feat : ADD Login, Logout, Signup util and Setting is done

--- stock/stockapp/urls.py
+++ stock/stockapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
+app_name = 'stock'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('signup/', views.registerPage, name='signup'),
 ]
--- stock/stockapp/views.py
+++ stock/stockapp/views.py
@@ -1,7 +1,36 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+
+from stock.stockapp.migrations.form import CreateUserForm
+
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form =CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'registration/Signup.html', context)
+
+class CustomLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'registration/Login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('stock:index')
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('stock:index')
+
